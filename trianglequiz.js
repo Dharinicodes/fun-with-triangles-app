d40d6ed1959e2eb3875dad1f215b3d48cb4b3e5d
Dharinicodes <126567741+Dharinicodes@users.noreply.github.com>
2023-05-20 19:49:33 +0530

    docs: changes added

--- trianglequiz.js
+++ trianglequiz.js
@@ -1,20 +1,36 @@
 const quizForm = document.querySelector(".quiz-form");
 const submitBtn = document.querySelector("#submit-btn");
-const output = document.querySelector("#output");
+const outputElement = document.querySelector("#output");
 
-const correctAnswers = ["90°", "right angled"];
+const correctAnswers = [
+  "90°",
+  "right angled",
+  "one right angle",
+  "12,16,20",
+  "Equilateral triangle",
+  "100°",
+  "30°",
+  "a + b + c",
+  "no",
+  "45°",
+];
 
 function calculateScore() {
-  let score = 0;
-  let index = 0;
   const formResults = new FormData(quizForm);
+  console.log("fr ", formResults);
+  let score = 0;
+  index = 0;
   for (let value of formResults.values()) {
+    console.log("index ", index);
+    console.log("ca ", correctAnswers[index]);
+    console.log("bv ", value);
     if (value === correctAnswers[index]) {
+      console.log("crrect ", value);
       score = score + 1;
     }
     index = index + 1;
   }
-  output.innerText="Your total score is " + score;
+  outputElement.innerText = "Your total score is " + score;
 }
 
 submitBtn.addEventListener("click", calculateScore);
